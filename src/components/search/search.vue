81a16fc02269ea2e6ddd74904ba5e8176898bfc5
Toneygg <745760128@qq.com>
2017-07-15 17:47:27 +0800

    888

---
+++ src/components/search/search.vue
@@ -0,0 +1,21 @@
+<template>
+  <Input v-model="value13" class='inputbox'>
+        <Button slot="append" icon="ios-search"></Button>
+    </Input>
+</template>
+<script>
+  export default {
+    name: 'search',
+    data () {
+      return {
+        value13: ''
+      }
+    }
+  }
+</script>
+<style lang="scss">
+  .inputbox{
+    width:200px;
+    
+  }
+</style>
--- src/components/student/home.vue
+++ src/components/student/home.vue
@@ -1,22 +1,75 @@
 <template>
-  <Table :columns="arr" :data="data"></Table>
+ <div>
+  <search></search>
+  <Table border :columns="arr" :data="data"></Table>
+ </div>
 </template>
 <script>
+  import search from '../search/search'
   export default {
     name: 'student-list',
+    components: {
+      search
+    },
     data () {
       return {
-        arr: [{
-          title: 1,
-          key: 'name'
-        }],
-        data: [{
-          name: 1
-        }]
+        arr: [
+          {
+            type: 'index',
+            title: '序 号',
+            width: 100,
+            fixed: 'left',
+            align: 'center'
+          },
+          {
+            title: '日 期',
+            key: 'date',
+            width: 200,
+            align: 'center'
+          },
+          {
+            title: '用 户 昵 称',
+            key: 'nickname',
+            width: 200,
+            align: 'center'
+          },
+          {
+            title: '姓 名',
+            key: 'name',
+            width: 200,
+            align: 'center'
+          },
+          {
+            title: '联 系 方 式',
+            key: 'telephone',
+            width: 200,
+            align: 'center'
+          },
+          {
+            title: '邮 箱',
+            key: 'zip',
+            width: 200,
+            align: 'center'
+          },
+          {
+            title: '操 作',
+            key: 'operation',
+            width: 200,
+            align: 'center'
+          }],
+        data: [
+          {
+            date: '2017-7-15',
+            nickname: 'Toney',
+            name: '孙超',
+            telephone: '15810392283',
+            zip: '745760128@qq.com'
+          }
+        ]
       }
     }
   }
 </script>
 <style lang="scss">
-  
+ 
 </style>
